c7dcba2c51af9bce601475d7cd1701447c786ca0
enjoy-life0000 <enjoylife.follower@gmail.com>
2025-01-25 13:54:26 -0500

    #1 font optimize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: 'jit', 
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        clash: ['Cabin Sketch"', 'sans-serif'], 
+        clash: ['Cabin Sketch"', 'sans-serif'],
       },
       fontSize: {
-        '10xl': '10rem', 
+        '10xl': '10rem',
         '11xl': '12rem',
-        '12xl': '14rem', 
-        '13xl': '16rem', 
+        '12xl': '14rem',
+        '13xl': '16rem',
       },
       colors: {
         orange: '#FFEFDA',
@@ -22,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
+};
